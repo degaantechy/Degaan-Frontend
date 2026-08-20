f771d2d0c0a267b6ced0c1469891ca298c560ee4
degaantechy <degaantech@gmail.com>
2026-08-20 09:31:37 +0300

    Use Montserrat across frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import '../styles/stylesglobals.css'
 import Layout from '../components/Layout'
+import { Montserrat } from 'next/font/google'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <ToastContainer position="bottom-right" />
-    </Layout>
+    <div className={montserrat.className}>
+      <Layout>
+        <Component {...pageProps} />
+        <ToastContainer position="bottom-right" />
+      </Layout>
+    </div>
   )
 }
 
