048c5ed839da819c45e6c878d5d6f1438bd0c596
Abdirisaq Mohamed <degaantech@gmail.com>
2026-09-11 01:02:54 +0300

    Use system typography and load minimal design layer

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,21 @@
 import '../styles/stylesglobals.css'
 import '../styles/brand-upgrade.css'
+import '../styles/apple-minimal.css'
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import GoogleAnalytics from '../components/GoogleAnalytics'
-import { Montserrat } from 'next/font/google'
 import { ToastContainer } from 'react-toastify'
 import { LanguageProvider } from '../contexts/LanguageContext'
 import 'react-toastify/dist/ReactToastify.css'
 
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-})
-
 function MyApp({ Component, pageProps }) {
   return (
     <LanguageProvider>
-      <div className={montserrat.className}>
+      <div>
         <Head>
           <link rel="icon" type="image/png" href="/images/degaan-mark.png" />
           <link rel="apple-touch-icon" href="/images/degaan-mark.png" />
-          <meta name="theme-color" content="#2c3240" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <Layout>
           <Component {...pageProps} />
