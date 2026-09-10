c1c4d228804ac08e854e3985f6c468756fe6db12
Abdirisaq Mohamed <degaantech@gmail.com>
2026-09-11 00:43:17 +0300

    Track SPA page views in Google Analytics

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,9 +1,29 @@
 import Script from 'next/script'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 const hasValidMeasurementId = /^G-[A-Z0-9]+$/i.test(measurementId || '')
 
 export default function GoogleAnalytics() {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!hasValidMeasurementId) return undefined
+
+    const trackPageView = (url) => {
+      if (typeof window.gtag !== 'function') return
+      window.gtag('event', 'page_view', {
+        page_path: url,
+        page_location: window.location.href,
+        page_title: document.title,
+      })
+    }
+
+    router.events.on('routeChangeComplete', trackPageView)
+    return () => router.events.off('routeChangeComplete', trackPageView)
+  }, [router.events])
+
   if (!hasValidMeasurementId) return null
 
   return (
